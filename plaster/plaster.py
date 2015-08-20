fe6bd3d9b705c5852a932c7dea1537815b509185
ampling <ampling@users.noreply.github.com>
2015-08-19 22:53:50 -0400

    Open Source

--- plaster/plaster.py
+++ plaster/plaster.py
@@ -1,5 +1,15 @@
 #! /usr/bin/env python3
 
+##############################################################################
+#
+# Copycenter (c) [2015-08-06], ISC, [Ampling]
+#
+##############################################################################
+""" Plaster
+
+Plaster is a friendly command-line pastebin client.
+"""
+
 import fileinput
 
 for line in fileinput.input():
