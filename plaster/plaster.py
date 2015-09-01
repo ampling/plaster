06f2ba24f917daeea6f53be37bd956c9d793d493
ampling <ampling@users.noreply.github.com>
2015-09-01 01:13:59 -0400

    Moved _load call to for loop.

--- plaster/plaster.py
+++ plaster/plaster.py
@@ -29,7 +29,6 @@ def read_config(head):
     config = configparser.ConfigParser()
     config.sections()
     config.read(config_file)
-    #config.sections()
     plugin = config.sections()[head]
     url = config[plugin]['url']
     #login =
@@ -42,29 +41,28 @@ def cull_plugin(): #add: uptime[default=0]
     '''Choose the best plugin for the job.'''
     plugin = read_config(0)[0] # expand
     url = read_config(0)[1]
-    best_plugin = prefix + plugin + ".py"
-    return (best_plugin, url)
+    path = prefix + plugin + ".py"
+    return (path, url)
 #}
 
 #{
 def scout_plugins(self):
     '''Search plugins folder for desired plugin.'''
     list_plugins = glob("plugins/*.py")
-    best_plugin = cull_plugin()[0]
-    if best_plugin not in list_plugins:
-        print("error: plugin " + best_plugin + " not found") #test me
+    path = cull_plugin()[0]
+    if path not in list_plugins:
+        print("error: plugin " + path + " not found") #test me
+    return path
 #}
 
 #{   
-def _load():
-    path = './plugins/sprunge_.py'
-    module_name = 'sprunge_'
-    spec = SourceFileLoader('plugin', path)
+def _load(plugin, path):
+    scout_plugins(plugin)
+    spec = SourceFileLoader(plugin, path)
     _plugin = spec.load_module()
     return _plugin
 #}
 
-_load()
 
 #from plugins import sprunge_ # expand
 #importlib.util.spec_from_file_location(module_name, filepath)
@@ -77,10 +75,10 @@ _load()
 #
 
 for payload in fileinput.input():
+    path = './plugins/sprunge_.py'
     plugin = cull_plugin()[0]
     url = cull_plugin()[1]
-    scout_plugins(plugin)
-    link = _load().posts(payload, url) # expand
+    link = _load(plugin, path).posts(payload, url) # expand
     if 'http' in link:
         print(link)
     
